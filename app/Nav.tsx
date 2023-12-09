98b020933de2cb5a96c1c07e7135fd80a256e547
endalk5728 <endalkengida8@gmail.com>
2023-12-09 01:16:28 -0800

    create navigation and style the active link in the nab bar

--- app/Nav.tsx
+++ app/Nav.tsx
@@ -1,12 +1,15 @@
-
+'use client'
 import Link from 'next/link';
 import React from 'react'
 import { AiFillBug } from "react-icons/ai";
+import { usePathname } from 'next/navigation';
+import classNames from 'classnames';
 
 const Nav = () => {
 
-const Links=[
-  {lable:'Dashboard',href:'/'},
+  const currentPath=usePathname();
+const links=[
+  {label:'Dashboard',href:'/'},
   {label:'Issues',href:'/issue'}
 
 ]
@@ -15,9 +18,38 @@ const Links=[
     <nav className='flex space-x-6 border-b mb-5 px-s h-14 items-center' >
     {/* add logo */}
     <Link href='/'><AiFillBug/></Link>
-<ul className='flex space-x-6'>
+    <ul className="flex space-x-6">
+      {links.map(link => (
+        <li key={link.href}>
+          <Link
+            className={classNames({
+              'text-zinc-900':link.href ===currentPath,
+              'text-zinc-500':link.href !==currentPath,
+              'hover:text-zinc-800 transition-colors':true
+            })}
+            href={link.href}
+          >
+            {link.label}
+            
+          </Link>
+        </li>
+      ))}
+    </ul>
+{/* {links.map(link=><Link key={link.href}
+className={
+classNames({
+  'text-zinc-900':link.href ===currentPath,
+  'text-zinc-500':link.href !==currentPath,
+  'hover:text-zinc-800 transition-colors':true
+})
 
-</ul>
+}>
+
+</Link>)} */}
+
+
+
+
     </nav>
     
 
